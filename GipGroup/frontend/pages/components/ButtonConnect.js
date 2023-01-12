05ed5d161d898d348ba9db8a91c0420ad1589905
TheSaint20010 <68438468+TheSaintUkraine@users.noreply.github.com>
2023-01-12 18:51:52 +0100

    Add files via upload

---
+++ GipGroup/frontend/pages/components/ButtonConnect.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ButtonConnect() {
+  return (
+    
+    <div className="button-connect">
+        <p>Обратный звонок</p>
+    </div>
+  )
+}
+
+export default ButtonConnect
